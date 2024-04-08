e4e85a4b1fc4f8db53ee4b6934153ea1cd574f41
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2024-04-08 22:09:05 +0300

    redact TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -11,25 +11,13 @@ export default function TransactionHistory({items}){
       <th className={css.border}>Currency</th>
     </tr>
   </thead>
-      {items.map(transact =>(
-          <tr className={(css.colorBgWhite, transact.id % 2 ? css.colorBgGrey : css.colorBgWhite)
-          } key={transact.id}>
-            <td className={css.cell}>{transact.type}</td>
-            <td className={css.cell}>{transact.amount}</td>
-            <td className={css.cell}>{transact.currency}</td>
+      {items.map((item) =>(
+          <tr className={css.bodyItem}>
+          <td className={css.cell}>{item.type}</td>
+          <td className={css.cell}>{item.amount}</td>
+          <td className={css.cell}>{item.currency}</td>
           </tr>
         ))}
 </table>
     );
     }
-
-    TransactionHistory.propTypes = {
-      items: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.string.isRequired,
-          type: PropTypes.string.isRequired,
-          amount: PropTypes.string.isRequired,
-          currency: PropTypes.string.isRequired,
-        })
-      ).isRequired,
-    };
